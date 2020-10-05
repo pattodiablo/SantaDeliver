8f2aecfee8ae151de531a2b06d71fe60b0fdf0a3
pattodiablo <pattodiablo@gmail.com>
2020-10-05 15:00:48 -0500

    asd

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 window.addEventListener('load', function () {
 
 	var game = new Phaser.Game({
-		width: 800,
-		height: 600,
+		width: 640, 
+		height: 960,
 		type: Phaser.AUTO,
-        backgroundColor: "#242424",
+        backgroundColor: "#000000",
 		scale: {
 			mode: Phaser.Scale.FIT,
 			autoCenter: Phaser.Scale.CENTER_BOTH
@@ -20,7 +20,7 @@ class Boot extends Phaser.Scene {
 
 	preload() {
 		
-		this.load.pack("pack", "assets/asset-pack.json");
+		this.load.pack("pack", "assets/asset-pack.json"); 
 	}
 
 	create() {
